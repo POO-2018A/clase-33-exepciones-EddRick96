4bfc6912b9f722c91e5d0f6f6435d0a928b98710
Erick Bolaños <38430620+EddRick96@users.noreply.github.com>
2018-08-11 22:51:36 -0500

    Add files via upload

--- Excepciones/src/excepciones/Excepciones.java
+++ Excepciones/src/excepciones/Excepciones.java
@@ -6,6 +6,8 @@
 package excepciones;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -17,29 +19,50 @@ public class Excepciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            System.out.println("Intentamos ejecutar el bloque de instrucciones:");
-            System.out.println("Instrucción 1.");
-            System.out.println("Instrucción 2.");
-            int n = Integer.parseInt("M"); //error forzado en tiempo de ejecución.
-            System.out.println("Instrucción 3, etc.");
+        
+       Scanner scan = new Scanner(System.in);
 
-//        } catch(NumberFormatException nfe){
-//            System.out.println("************************************************");
-//            System.out.println("Instrucciones a ejecutar cuando se produce un error de formato de numero");
-//            System.out.println(nfe.getMessage());
-//            System.out.println(Arrays.toString(nfe.getStackTrace()));
-//            
-        } catch (Exception e) {
-            System.out.println("************************************************");
-            System.out.println("Instrucciones a ejecutar cuando se produce un error de cuaquier otro tipo");
-            System.out.println(e.getMessage());
-            System.out.println(Arrays.toString(e.getStackTrace()));
+        try {
+            System.out.print("Ingrese el número de enteros que quiera asignar: ");
+            int num = scan.nextInt();
+            System.out.println("El número es: " + num);
+
+            int numeros[] = new int[num];
+                        
+            for(int i = 0; i < num; i++){
+                System.out.print("Ingrese el valor en la posición " + (i+1) +" :");
+                numeros[i] = scan.nextInt();
+            }
             
-        } finally {
-            System.out.println("************************************************");
-            System.out.println("Terminó el programa");
-            System.out.println("Instrucciones a ejecutar finalmente tanto si se producen errores como si no.");
+            System.out.print("Ingrese la posición que desee: ");
+            int posicion = scan.nextInt();
+            
+            System.out.println("La posicion que seleccionó es: " + numeros[posicion-1]);
+
+        } 
+        catch (Exception excp1) {
+            System.out.println("Por favor, ingrese solo números eso es una letra.");
+        } 
+        
+        //Ejemplo dos
+        
+        try {
+
+            System.out.print("Ingrese el primer valor: ");
+            int dato1 = scan.nextInt();
+
+            System.out.print("Ingrese el segundo valor: ");
+            int dato2 = scan.nextInt();
+            
+            int cociente = dato1/dato2;
+            System.out.println("El valor del cociente es: "+ cociente);
+
+        } catch (InputMismatchException  ime) {
+            System.out.println("Por favor, ingrese solo números eso es una letra.");
+            
+        }catch (ArithmeticException ae) {
+            System.out.println("No existe división para cero ! :v");
+            
         }
     }
     
